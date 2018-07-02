0d98d57e32a3246a98f75857d30de4c264d79028
comeyoung <18311448784@163.com>
2018-07-02 09:47:45 +0800

    change

--- src/main/java/org/edi/stocktask/repository/BOReposirotyStockReport.java
+++ src/main/java/org/edi/stocktask/repository/BOReposirotyStockReport.java
@@ -4,6 +4,7 @@ import org.edi.stocktask.bo.stockreport.StockReport;
 import org.edi.stocktask.bo.stockreport.StockReportItem;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.edi.stocktask.mapper.StockReportMapper;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -17,52 +18,41 @@ import java.util.List;
 public class BOReposirotyStockReport implements IBOReposirotyStockReport{
 
     @Resource
-    private IBOReposirotyStockReport iBOReposirotyStockReport;
+    private StockReportMapper stockReportMapper;
 
 
+    @Override
     //查询库存任务汇报
     public List<StockReport> fetchStockReport(){
-        List<StockReport> StockReports = iBOReposirotyStockReport.fetchStockReport();
+        List<StockReport> StockReports = stockReportMapper.fetchStockReport();
         for(int i=0;i<StockReports.size();i++){
             StockReport stockReport = StockReports.get(i);
-            List<StockReportItem> StockReportItems = iBOReposirotyStockReport.fetchStockReportItem(stockReport.getDocEntry());
+            List<StockReportItem> StockReportItems = stockReportMapper.fetchStockReportItem(stockReport.getDocEntry());
             stockReport.setStockReportItems(StockReportItems);
         }
         return StockReports;
     }
 
 
+    @Override
     //根据DOCENTRY查询库存任务汇报
     public List<StockReport> fetchStockReportByEntry(Integer docEntry){
-        List<StockReport> StockReports = iBOReposirotyStockReport.fetchStockReportByEntry(docEntry);
+        List<StockReport> StockReports = stockReportMapper.fetchStockReportByEntry(docEntry);
         return StockReports;
     }
 
 
-    //根据DOCENTRY查询库存任务汇报明细
-    public List<StockReportItem> fetchStockReportItem(Integer docEntry){
-        List<StockReportItem> StockReportItems = iBOReposirotyStockReport.fetchStockReportItem(docEntry);
-        return StockReportItems;
-    }
 
-
-
-   //保存库存任务汇报
+    @Override
+    //保存库存任务汇报
     public void saveStockReport(StockReport stockReport){
         try {
-            iBOReposirotyStockReport.saveStockReport(stockReport);
+            stockReportMapper.saveStockReport(stockReport);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //保存库存任务汇报明细
-    public void saveStockReportItem(StockReportItem stockReportItem){
-        try {
-            iBOReposirotyStockReport.saveStockReportItem(stockReportItem);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 
 }
