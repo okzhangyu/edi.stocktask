a63e35581ac3735504713f60e3c4eac0141ff1f8
comeyoung <18311448784@163.com>
2018-07-30 12:56:35 +0800

    Revert "序列修改"
    This reverts commit 24d804cc2cd9fa36cbf0c8ee310160273a3807fa.

--- src/main/resources/sql/Seq_DocEntry.sql
+++ src/main/resources/sql/Seq_DocEntry.sql
@@ -7,10 +7,10 @@ GO
 /****** Object:  Sequence [dbo].[Seq_DocEntry]    Script Date: 2018/7/30 10:23:27 ******/
 CREATE SEQUENCE [dbo].[Seq_DocEntry]
  AS [bigint]
- START WITH 1
+ START WITH 200
  INCREMENT BY 1
- MINVALUE 1
- MAXVALUE 20000
+ MINVALUE 100
+ MAXVALUE 2000
  CYCLE
  CACHE  3
 GO
