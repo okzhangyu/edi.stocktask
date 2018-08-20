1e468b9c0857690a8b486744ec2da652a0285603
fancys <fancy553973204@gmail.com>
2018-08-20 17:16:22 +0800

    实现批量条码解析

--- src/main/java/org/edi/stocktask/dto/CodeBarResult.java
+++ src/main/java/org/edi/stocktask/dto/CodeBarResult.java
@@ -10,42 +10,52 @@ public class CodeBarResult implements ICodeBarResult {
     private String itemCode;
     private String codeBar;
 
+    @Override
     public String getBaseType() {
         return baseType;
     }
 
+    @Override
     public void setBaseType(String baseType) {
         this.baseType = baseType;
     }
 
+    @Override
     public Integer getBaseEntry() {
         return baseEntry;
     }
 
+    @Override
     public void setBaseEntry(Integer baseEntry) {
         this.baseEntry = baseEntry;
     }
 
+    @Override
     public Integer getBaseLine() {
         return baseLine;
     }
 
+    @Override
     public void setBaseLine(Integer baseLine) {
         this.baseLine = baseLine;
     }
 
+    @Override
     public String getItemCode() {
         return itemCode;
     }
 
+    @Override
     public void setItemCode(String itemCode) {
         this.itemCode = itemCode;
     }
 
+    @Override
     public String getCodeBar() {
         return codeBar;
     }
 
+    @Override
     public void setCodeBar(String codeBar) {
         this.codeBar = codeBar;
     }
